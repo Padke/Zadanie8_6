7932fcf633ac7c867200c83fa3d834c3c9ab9fad
Patrycja Kania <el.czupakabra21@gmail.com>
2018-03-08 14:58:10 +0100

    Add content to script.js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var a = prompt('enter a value:');
+var b = prompt('enter b value:');
+var value = (a * a) - (2 * a * b) - (b * b);
+
+console.log("tyle wychodzi z działania: " + value);
+
+if (value > 0){
+    console.log ('wynik dodatni');
+}
+else if (value = 0 ){'wynik nieujemny i niedodatni, czyli 0'}
+else {
+     console.log ('wynik ujemny');
+}
